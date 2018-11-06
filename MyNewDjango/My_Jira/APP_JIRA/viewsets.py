af54e80349c42de42e4961ece38edd9d703588cb
GVSHABAREESH <42208520+GVSHABAREESH@users.noreply.github.com>
2018-11-06 15:31:58 +0530

    Delete viewsets.py

--- MyNewDjango/My_Jira/APP_JIRA/viewsets.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from .serializers import MyClassSerializer
-from .models import MyClass
-
-class MyClassviewsets(viewsets.ModelViewSet):
-
-    serializer_class = MyClassSerializer
-    queryset = MyClass.objects.all()
-
